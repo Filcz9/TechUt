e4c3ecc63bdb89bf65cae01b05ae34d99e082c4d
Filcz9 <filcz626@gmail.com>
2019-01-09 17:38:25 +0100

    Update zadania 5

---
+++ zad05/src/test/java/com/fczyz/service/ComputerGameManagerTest.java
@@ -0,0 +1,28 @@
+package com.fczyz.service;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fczyz.domain.ComputerGame;
+import com.fczyz.domain.ActivationKey;
+
+import com.fczyz.service.ComputerGameManager;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:/beans.xml" })
+@TransactionConfiguration(transactionManager = "txManager", defaultRollback = false)
+@Transactional
+public class ComputerGameManagerTest {
+
+}
